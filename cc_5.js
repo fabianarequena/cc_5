e26d0ea045dc5e0f63e635a67f94ee185ce5dabd
fabianarequena <fabianarequena@usf.edu>
2025-02-07 08:33:32 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,15 @@
+// Task 1: Customer Profile
+
+let customer = {
+    name: "Sophia Kirschner",
+    age: 21,
+    email: "bophia@umiami.edu"
+};
+// Declared a customer by using a constant so that it cant be reassigned
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
+// Logged using template literals
+
