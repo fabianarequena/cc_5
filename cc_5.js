7ec8801ed57f90d88f64e035b23e6c48ba2bfcfe
fabianarequena <fabianarequena@usf.edu>
2025-02-08 16:51:10 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -5,11 +5,29 @@ let customer = {
     age: 21,
     email: "bophia@umiami.edu"
 };
-// Declared a customer by using a constant so that it cant be reassigned
+// Declared a customer 
 
 console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
 
 // Logged using template literals
+
+
+// Task 2: Order Details
+
+let order = {
+    orderId: 007,
+    totalAmount: 275,
+    status: "Shipment in Process",
+    displayOrder: function () {
+        console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+};
+// Declared order details with this function
+
+
+order.displayOrder();
+
+// Display details provided
 
